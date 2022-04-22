151c912e6fe51fa683cca0ec3e4901edab194f55
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-22 04:57:23 -0400

    implemented mock potion

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,14 @@
 // when requiring modules, it is not necessary to include the .js file extension; node will assume that the file is a JS file is no extension is specified
+const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
+// imports the Potion() constructor into the test; thus, establishing Potion as a usable variable (otherwise, new Potion() would throw an error)
+const Potion = require('../lib/Potion');
+
+// mocks/replaces the constructor's implementation with our faked data; if new Potion() is ever called within the test file itself or any of the subsequent modules attached to the test, the mocked data will be returned
+jest.mock('../lib/Potion');
+
+// the name will always be 'health' and the value will always be 20 because the mocked potion replaced the real Potion() constructor
+console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -8,6 +17,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    // player's inventory should be an array containing an object
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
 // to run a particular group of related tests (known as a test suite), modify the npm run command to call specific test files (i.e. npm run test Player)
