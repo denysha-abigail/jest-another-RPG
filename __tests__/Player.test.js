fc251f329553483c61efc266357242d9271b202d
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-23 04:32:33 -0400

    added getStats() and getInventory(); implemented prototype syntax

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,4 @@
 // when requiring modules, it is not necessary to include the .js file extension; node will assume that the file is a JS file is no extension is specified
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 // imports the Potion() constructor into the test; thus, establishing Potion as a usable variable (otherwise, new Potion() would throw an error)
 const Potion = require('../lib/Potion');
@@ -22,3 +21,22 @@ test('creates a player object', () => {
 });
 
 // to run a particular group of related tests (known as a test suite), modify the npm run command to call specific test files (i.e. npm run test Player)
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
