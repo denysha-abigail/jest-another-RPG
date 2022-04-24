29fdf97734f0f23d79f69aff5077919677266ea4
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-24 05:02:26 -0400

    added Player object methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -39,4 +39,39 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+// test to get information about the player's health
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    // concatenation is joining together multiple strings into a single string; the expect.stringContaining() method is an expect method that can be used to make sure our string includes our player's health
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    // here we are updating our Player health halfway through the test so that we can check for both conditions: true and false
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+// reduceHealth() method is called twice
+// a new Player is created in every test; our tests affect the Player object's property values and if we used the same object every time, we would no longer be testing properties and methods in isolation; essentially, it's important to create a new instance of the object we're testing in every test to give that test a fresh start
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    // absurdly high value to make sure it never goes negative
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
